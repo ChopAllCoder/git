357edf410f12758a967282020b9630c9c6f6fe0e
MengLE <982993969@qq.com>
2022-05-19 15:28:55 +0800

    v1 第一次修改

---
+++ src/main/java/cn/ac/istic/git/GitTest.java
@@ -0,0 +1,13 @@
+package cn.ac.istic.git;
+
+/**
+ * @author Meng L'E
+ * @create 2022-05-19-15:25
+ * @Description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git ! meng");
+    }
+
+}
